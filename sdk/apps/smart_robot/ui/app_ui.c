d399a28bb69190be52f8269b89cf0abd24f1bb49
waynelu <luyongfugx@163.com>
2026-02-26 14:46:22 +0800

    add happ gif

--- sdk/apps/smart_robot/ui/app_ui.c
+++ sdk/apps/smart_robot/ui/app_ui.c
@@ -32,6 +32,8 @@ static char m_content_text[512] = {0};
 
 extern unsigned char emotion_wink_gif[];
 extern unsigned int wink_gif_len;
+extern unsigned char emotion_happy_gif[];
+extern unsigned int happy_gif_len;
 
 void ui_set_status_text(const char *text)
 {
@@ -105,8 +107,8 @@ int lvgl_v9_main_task_hook()
 
         if( !m_emotion_gif ){
             static lv_image_dsc_t s_emotion_wink_gif_dsc;
-            s_emotion_wink_gif_dsc.data = emotion_wink_gif;
-            s_emotion_wink_gif_dsc.data_size = wink_gif_len;
+            s_emotion_wink_gif_dsc.data = emotion_happy_gif;
+            s_emotion_wink_gif_dsc.data_size = happy_gif_len;
             s_emotion_wink_gif_dsc.header.magic = LV_IMAGE_HEADER_MAGIC;
             s_emotion_wink_gif_dsc.header.flags = 0;
             s_emotion_wink_gif_dsc.header.cf = LV_COLOR_FORMAT_RAW;
