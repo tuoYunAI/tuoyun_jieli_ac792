101d42e1fb08e19af545a4b80d4b4b8477bf7611
waynelu <luyongfugx@163.com>
2026-02-26 15:29:32 +0800

    chage  emotion

--- sdk/apps/smart_robot/ui/lvgl_v9/lv_conf.h
+++ sdk/apps/smart_robot/ui/lvgl_v9/lv_conf.h
@@ -623,26 +623,26 @@
 /*Montserrat fonts with ASCII range and some symbols using bpp = 4
  *https://fonts.google.com/specimen/Montserrat*/
 #define LV_FONT_MONTSERRAT_8  1
-#define LV_FONT_MONTSERRAT_10 1
-#define LV_FONT_MONTSERRAT_12 1
-#define LV_FONT_MONTSERRAT_14 1
-#define LV_FONT_MONTSERRAT_16 1
-#define LV_FONT_MONTSERRAT_18 1
-#define LV_FONT_MONTSERRAT_20 1
-#define LV_FONT_MONTSERRAT_22 1
-#define LV_FONT_MONTSERRAT_24 1
-#define LV_FONT_MONTSERRAT_26 1
-#define LV_FONT_MONTSERRAT_28 1
-#define LV_FONT_MONTSERRAT_30 1
-#define LV_FONT_MONTSERRAT_32 1
-#define LV_FONT_MONTSERRAT_34 1
-#define LV_FONT_MONTSERRAT_36 1
-#define LV_FONT_MONTSERRAT_38 1
-#define LV_FONT_MONTSERRAT_40 1
-#define LV_FONT_MONTSERRAT_42 1
-#define LV_FONT_MONTSERRAT_44 1
-#define LV_FONT_MONTSERRAT_46 1
-#define LV_FONT_MONTSERRAT_48 1
+// #define LV_FONT_MONTSERRAT_10 1
+// #define LV_FONT_MONTSERRAT_12 1
+// #define LV_FONT_MONTSERRAT_14 1
+// #define LV_FONT_MONTSERRAT_16 1
+// #define LV_FONT_MONTSERRAT_18 1
+// #define LV_FONT_MONTSERRAT_20 1
+// #define LV_FONT_MONTSERRAT_22 1
+// #define LV_FONT_MONTSERRAT_24 1
+// #define LV_FONT_MONTSERRAT_26 1
+// #define LV_FONT_MONTSERRAT_28 1
+// #define LV_FONT_MONTSERRAT_30 1
+// #define LV_FONT_MONTSERRAT_32 1
+// #define LV_FONT_MONTSERRAT_34 1
+// #define LV_FONT_MONTSERRAT_36 1
+// #define LV_FONT_MONTSERRAT_38 1
+// #define LV_FONT_MONTSERRAT_40 1
+// #define LV_FONT_MONTSERRAT_42 1
+// #define LV_FONT_MONTSERRAT_44 1
+// #define LV_FONT_MONTSERRAT_46 1
+// #define LV_FONT_MONTSERRAT_48 0
 
 /* Demonstrate special features */
 #define LV_FONT_MONTSERRAT_28_COMPRESSED 1  /*bpp = 3*/
@@ -653,7 +653,7 @@
 #define LV_FONT_SOURCE_HAN_SANS_SC_16_CJK   1  /**< 1338 most common CJK radicals */
 
 /** Pixel perfect monospaced fonts */
-#define LV_FONT_UNSCII_8  1
+#define LV_FONT_UNSCII_8  0
 #define LV_FONT_UNSCII_16 1
 
 /** Optionally declare custom fonts here.
